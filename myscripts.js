9033f638100f7902b284d7f2833cde0ed894af55
Atheer <63922425+Soft-Eng-Soft@users.noreply.github.com>
2025-05-15 17:10:38 +0300

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,41 +1,36 @@
 var AppID =0;
+var ProjectID="";
+var tody ;
 
 
 window.onload = function () {
 
-  // Add_Ui(22);
 
-/*
+  var d = new Date();
+   tody = d.getDate()  + "-" + (d.getMonth()+1) + "-" + d.getFullYear();
+
+  document.getElementById("txtdate").innerHTML = tody;
+
   if (typeof (Storage) !== "undefined") {
-    document.getElementById("ProjectID").value = localStorage.getItem("FirebaseID");
-    document.getElementById("Dr_Name").value = localStorage.getItem("DrName");
+
+    document.getElementById("ProjectID").style.display = 'none';
+    document.getElementById("btnvaseID").style.display = 'none';
+  document.getElementById("btnconn").style.display = 'flex';
+   
+ 
+    ProjectID = localStorage.getItem("FirebaseID");   
+   
+    var baseurl = ProjectID + "/" + tody  + ".json";
+   // console.log(baseurl);
+
+   removolddate(ProjectID);
 
   }
 
-
-  if (document.getElementById("ProjectID").value !== "") {
-
-    var ProjectID = document.getElementById("ProjectID").value;
-    var drname = document.getElementById("Dr_Name").value;
-
-
-    var baseurl = ProjectID + drname + ".json";
-
-    //var baseurl = "https://"+ProjectID+".asia-southeast1.firebasedatabase.app/control1.json";
-
-    /*		var baseurl = "https://"+ProjectID+".firebaseio.com/Control.json";
-        <!--  var baseurl = "https://maui-db-default-rtdb.asia-southeast1.firebasedatabase.app/cmd.json";
-
-
-    GetState(baseurl);
-  }
-
-  */
-
-
-  var baseurl = "https://app-db-df0f1-default-rtdb.asia-southeast1.firebasedatabase.app/Dr-Marthad.json";
+ // var baseurl = "https://app-db-df0f1-default-rtdb.asia-southeast1.firebasedatabase.app/Dr-Marthad.json";
   GetName(baseurl);  
 
+  /*
   var xmlHttp = new XMLHttpRequest();
   var UpdateVisitsURL = "https://iotcar-2767a.firebaseio.com/FirebaseHomeVisits.json";
 
@@ -48,12 +43,69 @@ window.onload = function () {
   newxmlHttp.open("PUT", UpdateVisitsURL, true);
   newxmlHttp.setRequestHeader('Content-type', 'text/plain;');
   newxmlHttp.send(visits);
+  */
 }
 
 
 
+function saveID() {
+
+  if (document.getElementById("ProjectID").value !== "") {
+    ProjectID = document.getElementById("ProjectID").value;
+ 
+  if (typeof (Storage) !== "undefined") {
+    localStorage.setItem("FirebaseID", document.getElementById("ProjectID").value);
+    document.getElementById("ProjectID").style.display = 'none';
+    document.getElementById("btnvaseID").style.display = 'none';
+   
+  // var baseurl = ProjectID  + "/" + tody  + ".json";
+  // console.log(baseurl);
+
+   // GetName(baseurl);
+  } else {
+    document.getElementById("error").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+  
+  }
+}
+
+function showfire(){
+  document.getElementById("ProjectID").style.display = 'flex';
+  document.getElementById("btnvaseID").style.display = 'flex'; 
+  document.getElementById("ProjectID").value = localStorage.getItem("FirebaseID");   
+
+}
 
 
+
+function removolddate(Url){
+
+ // console.log(Url + ".json");
+  var xmlHttp = new XMLHttpRequest();
+
+  xmlHttp.open("GET", Url + ".json", false); // false for synchronous request
+
+  xmlHttp.send(null);
+
+// console.log(xmlHttp.responseText);
+
+var obj = JSON.parse(xmlHttp.responseText);
+
+var dv = []
+for (var k in obj) {
+  dv.push(k);
+ // console.log(k);
+  if (k !== tody)
+  {
+    xmlHttp.open("DELETE", Url +"/" + k + ".json", false); // false for synchronous request
+    xmlHttp.send(null);
+  // console.log("ok");
+  }
+  
+}
+
+  //console.log(Url.replace(ProjectID,""));
+}
 
 function GetName(Url) {
   var xmlHttp = new XMLHttpRequest();
@@ -61,7 +113,6 @@ function GetName(Url) {
   xmlHttp.open("GET", Url, false); // false for synchronous request
 
   xmlHttp.send(null);
-
  
   var obj = JSON.parse(xmlHttp.responseText);
 
@@ -70,7 +121,7 @@ function GetName(Url) {
     AppID = obj.length - 1 ;
   }
   
- document.getElementById("error").innerHTML = AppID;
+ 
 
   var dv = []
   for (var k in obj) {
@@ -124,10 +175,10 @@ for (let k=0; k< tm.options.length; k++)
 }
 
 
-console.log(appary.length);
+//console.log(appary.length);
 
   for (let i = 0; i < appary.length ; i++) {
-    console.log(nameary[i]);   
+   // console.log(nameary[i]);   
     const a = document.createElement('a');
     a.setAttribute('class', 'button');
    
@@ -142,10 +193,6 @@ a.innerHTML = nameary[i]  + " ......  " +  slctary[i]   ;
      document.getElementById("name").value = nm;
      document.getElementById("phone").value =  phn;
     
-
-//console.log(appid);
-//console.log(nm);
-//console.log(phn);
 
     });
 
@@ -167,8 +214,12 @@ a.innerHTML = nameary[i]  + " ......  " +  slctary[i]   ;
   var tmval = tim.value;
 
 var data = tmval + ";" + name + ";" + phone ;
-var datenow = Date.now.toString("yyyy-mm-dd");
-  var baseurl = "https://app-db-df0f1-default-rtdb.asia-southeast1.firebasedatabase.app/Dr-Marthad/" ;
+
+var baseurl = ProjectID  + "/" + tody  + "/";
+
+console.log(baseurl);
+
+ // var baseurl = "https://app-db-df0f1-default-rtdb.asia-southeast1.firebasedatabase.app/Dr-Marthad/" ;
 
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function () {
@@ -188,16 +239,14 @@ var datenow = Date.now.toString("yyyy-mm-dd");
 
 let delayres = await delay(2000);
 
- var nurl = "https://app-db-df0f1-default-rtdb.asia-southeast1.firebasedatabase.app/Dr-Marthad.json";
+ //var nurl = "https://app-db-df0f1-default-rtdb.asia-southeast1.firebasedatabase.app/Dr-Marthad.json";
 // document.getElementById("error").innerHTML = baseurl + ".json";
+var nurl = ProjectID  + "/" + tody  + ".json";
 
 
-
- document.getElementById("error").innerHTML = AppID;
  document.getElementById("name").value = "";
  document.getElementById("phone").value = "";
- //document.getElementById("name").innerHTML = "";
- //document.getElementById("phone").innerHTML = "";
+ 
  GetName(nurl);  
 
 }
@@ -207,27 +256,4 @@ const delay = (delayInms) => {
 };
 
 
-
-function saveID() {
-  if (typeof (Storage) !== "undefined") {
-    localStorage.setItem("FirebaseID", document.getElementById("ProjectID").value);
-    localStorage.setItem("DrName", document.getElementById("Dr_Name").value);
-
-    if (document.getElementById("ProjectID").value !== "") {
-      var ProjectID = document.getElementById("ProjectID").value;
-      var drname = document.getElementById("Dr_Name").value;
-
-      var baseurl = ProjectID + drname + ".json";
-
-      // baseurl = "https://"+ProjectID+".asia-southeast1.firebasedatabase.app/control1.json";
-
-      /*		<!--  var baseurl = "https://maui-db-default-rtdb.asia-southeast1.firebasedatabase.app/cmd.json";
-              var   var baseurl = "https://"+ProjectID+".firebaseio.com/cmd.json"; */
-
-      GetState(baseurl);
-    }
-  } else {
-    document.getElementById("error").innerHTML = "Sorry, your browser does not support Web Storage...";
-  }
-}
 
